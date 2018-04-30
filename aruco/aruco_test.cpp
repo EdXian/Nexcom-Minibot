4ecca2e7448916fed2a39e764d2babcdab5ddeff
EdXian <f29993856@gmail.com>
2018-04-30 17:53:46 +0800

    regroup

--- aruco/aruco_test.cpp
+++ aruco/aruco_test.cpp
@@ -6,18 +6,53 @@ using namespace aruco;
 
 void aruco_thread() {
 
-	cv::VideoCapture* capture = 0;
+	cv::VideoCapture* capture;
 	cv::Mat frame;
 	int width = 640;
 	int height = 480;
-	int camera_id = 1;
+	int camera_id = 0;
 
 	//使用第一個相機   
 	capture = new cv::VideoCapture(camera_id);
 	capture->set(CV_CAP_PROP_FRAME_WIDTH, width);
 	capture->set(CV_CAP_PROP_FRAME_HEIGHT, height);
+	capture->set(CV_CAP_PROP_FPS, 60);
 	int fps = capture->get(CAP_PROP_FPS);
 	printf("Frame rate : %d \n", fps);
+	Ptr<aruco::DetectorParameters> detect_parameter = DetectorParameters::create();
+	/*detect_parameter->adaptiveThreshWinSizeStep = 1;
+	detect_parameter->adaptiveThreshWinSizeMax = 6;
+	detect_parameter->adaptiveThreshWinSizeMin = 4;
+
+	detect_parameter->maxMarkerPerimeterRate = 2.5;
+	detect_parameter->minMarkerPerimeterRate = 0.1;
+	detect_parameter->polygonalApproxAccuracyRate = 0.04;
+	std::cout << detect_parameter->minCornerDistanceRate << std::endl;
+	std::cout << detect_parameter->minMarkerDistanceRate << std::endl;
+	std::cout << detect_parameter->minDistanceToBorder << std::endl;
+	detect_parameter->minCornerDistanceRate = 0.05;
+	detect_parameter->minMarkerDistanceRate = 0.05;
+	detect_parameter->minDistanceToBorder = 3;
+	detect_parameter->markerBorderBits =1;
+
+
+	detect_parameter->minOtsuStdDev = 5.0;
+	detect_parameter->perspectiveRemoveIgnoredMarginPerCell = 0.13;
+	
+	detect_parameter->maxErroneousBitsInBorderRate = 0.35;
+	detect_parameter->errorCorrectionRate = 0.6;
+
+	detect_parameter->cornerRefinementMethod = CORNER_REFINE_SUBPIX;
+	detect_parameter->cornerRefinementWinSize = 5;
+
+	detect_parameter->cornerRefinementMaxIterations = 30;
+	detect_parameter->cornerRefinementMinAccuracy = 0.1;*/
+
+	std::cout << detect_parameter->adaptiveThreshWinSizeStep <<"  "
+		<< detect_parameter->adaptiveThreshWinSizeMax << "  "
+		<< detect_parameter->adaptiveThreshWinSizeMin << "  "
+		<< std::endl;
+
 
 	while (true) {
 		//放入數據
@@ -33,14 +68,12 @@ void aruco_thread() {
 
 		std::vector<int> ids;     //Marker 的id
 		std::vector<std::vector<cv::Point2f> > corners;
-		cv::aruco::detectMarkers(image, dictionary, corners, ids);  //這個是偵測Aruco的API
-
-																	//以下為相機的校正 ， 應該先校正再做量測
-																	//cout << "num " << ids.size() << endl;
+		//cv::aruco::detectMarkers(image, dictionary, corners, ids,detect_parameter);  //這個是偵測Aruco的API
+		cv::aruco::detectMarkers(image, dictionary, corners, ids);
+			//以下為相機的校正 ， 應該先校正再做量測																						//cout << "num " << ids.size() << endl;
 		Mat cameraMatrix = (Mat_<double>(3, 3) << 826.323941, 0.000000, 330.817699, 0, 827.488990, 261.420381, 0.000, 0.000, 1.0000);
 		Mat distCoeffs = (Mat_<double>(1, 4) << 0.012823, -0.139212, 0.000502, -0.000537);
-
-
+		
 		if (ids.size() > 0)
 		{
 			//cout << "num " << ids.size() << endl;
@@ -79,7 +112,7 @@ int main() {
 	std::cout << "test" << std::endl;
 	
 	first.join();
-	system("pause");
+	//system("pause");
 
 
 	return 0;
